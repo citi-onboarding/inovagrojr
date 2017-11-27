5a9fb94ee2e9ed706c0caebe96de063a26a09b8c
Pamella <pbsf2@cin.ufpe.br>
2017-11-27 17:05:56 -0300

    navbar fixa ao usar scroll

--- mysite/core/templates/js/index.js
+++ mysite/core/templates/js/index.js
@@ -1,3 +1,4 @@
+// responsive navbar
 function navbarResponsive() {
 	var x = document.getElementById("inicio");
 	if (x.className === "navbar") {
@@ -6,3 +7,21 @@ function navbarResponsive() {
 	    x.className = "navbar";
 	}
 }
+
+// sticky navbar on scroll
+const nav = document.querySelector('#inicio');
+const navTop = nav.offsetTop;
+
+function stickyNavigation() {
+	console.log('navTop = ' + navTop);
+	console.log('scrollY = ' + window.scrollY);
+	if (window.scrollY >= navTop) {
+		/*document.body.style.paddingTop = nav.offsetHeight + 'px';*/
+		document.body.classList.add('fixed-nav');
+	} else {
+		/*document.body.style.paddingTop = 0;*/
+		document.body.classList.remove('fixed-nav');
+	}
+}
+
+window.addEventListener('scroll', stickyNavigation);
