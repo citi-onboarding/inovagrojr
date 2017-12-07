041ca042b90234ce69a92fc4940a8eaa39c4534e
Pamella <pbsf2@cin.ufpe.br>
2017-12-07 02:58:49 -0300

    incorporação do mapa

--- mysite/core/templates/js/index.js
+++ mysite/core/templates/js/index.js
@@ -31,3 +31,16 @@ function stickyNavigation() {
 }
 
 window.addEventListener('scroll', stickyNavigation);
+
+// google map api
+/*function initMap() {
+    var uluru = {lat: -8,0524, lng: -34,9454};
+    var map = new google.maps.Map(document.getElementById('idMap'), {
+      zoom: 4,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+}*/
